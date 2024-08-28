34cf75a9e95d0b662140d90c69d4932a31ac3a50
abdouchirac2024 <abdouchirac411@gmail.com>
2024-08-28 18:30:35 +0200

    correction de l'authentifaction

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@
 export default {
     providers: [
       {
-        domain: "https://fine-colt-47.clerk.accounts.dev",
+        domain: "https://deciding-seasnail-7.clerk.accounts.dev/",
         applicationID: "convex",
       },
     ]
